a4eb7369d754972f2907536241a35496468dd2d5
fhermas22 <franciscohermas@gmail.com>
2025-01-15 23:43:55 +0100

    Dynamisation et Finalisation de la visionneuse d'image (partie Lightbox)

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const imagesWrapper = document.querySelector(".images");
 const loadMoreBtn = document.querySelector(".load-more");
-const searchInput = document.querySelector(".search-box");
+const searchInput = document.querySelector(".search-box input");
+const lightBox = document.querySelector(".lightbox");
+const closeBtn = lightBox.querySelector(".fa-times");
+const downloadImgBtn = lightBox.querySelector(".fa-download");
 
 // API key, pagination, searchTerm variables
 const apiKey = "t4gbibwWyG7qscriaE054bFIMEV39wJc0GwgD8tbHHCqfyv7pD6zsYBB";
@@ -8,6 +11,7 @@ const perPage = 15;
 let currentPage = 1;
 let searchTerm = null;
 
+// Image Download event
 const downloadImg = (imgURL) => {
     fetch(imgURL).then(res => res.blob()).then(file => {
         const a = document.createElement("a");
@@ -17,17 +21,31 @@ const downloadImg = (imgURL) => {
     }).catch(() => alert("Impossible de télécharger l'image !"));
 }
 
+const showLightBox = (name, img) => {
+    // Showing lightbox and setting img source, name and button attribute
+    lightBox.querySelector("img").src = img;
+    lightBox.querySelector("span").innerText = name;
+    downloadImgBtn.setAttribute("data-img", img);
+    lightBox.classList.add("show");
+    document.body.style.overflow = "hidden";
+}
+
+const hideLightbox = () => {
+    lightBox.classList.remove("show");
+    document.body.style.overflow = "auto";
+}
+
 const generateHTML = (images) => {
     // Making li of all fetched images and adding them to the existing image wrapper
     imagesWrapper.innerHTML += images.map(img => 
-        `<li class="card">
+        `<li class="card" onclick="showLightBox('${img.photographer}', '${img.src.large2x}')">
                 <img src="${img.src.large2x}" alt="image">
                 <div class="details">
                     <div class="photographer">
                         <i class="fas fa-camera"></i>
                         <span>${img.photographer}</span>
                     </div>
-                    <button onclick="downloadImg('${img.src.large2x}')">
+                    <button onclick="downloadImg('${img.src.large2x}');event.stopPropagation();">
                         <i class="fas fa-download"></i>
                     </button>
                 </div>
@@ -71,3 +89,5 @@ const loadSearchImages = (e) => {
 getImages(`https://api.pexels.com/v1/curated?page=${currentPage}&per_page=${perPage}`);
 loadMoreBtn.addEventListener("click", loadMoreImages);
 searchInput.addEventListener("keyup", loadSearchImages);
+closeBtn.addEventListener("click", hideLightbox);
+downloadImgBtn.addEventListener("click", (e) => downloadImg(e.target.dataset.img));
